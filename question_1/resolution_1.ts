b4fd9a38feb8b9dbae2ce876125492fada9a9593
Emerson Bezerra <emersonbbezerra@gmail.com>
2025-01-11 17:35:42 -0300

    feat: :sparkles: add resolution_1 for question 1

---
+++ question_1/resolution_1.ts
@@ -0,0 +1,42 @@
+// Analisando o trecho de código:
+
+/*
+int INDICE = 13, SOMA = 0, K = 0;
+Enquanto K < INDICE faça { 
+    K = K + 1; 
+    SOMA = SOMA + K; 
+}
+Imprimir(SOMA);
+*/
+
+// Neste trecho de código, a variável SOMA será incrementada pela soma dos números de 1 a 13.
+
+// Inicialmente, SOMA é 0 e K é 0.
+
+// Quando K = 1, SOMA = 0 + 1 = 1
+
+// Quando K = 2, SOMA = 1 + 2 = 3
+
+// Quando K = 3, SOMA = 3 + 3 = 6
+
+// Quando K = 4, SOMA = 6 + 4 = 10
+
+// Quando K = 5, SOMA = 10 + 5 = 15
+
+// Quando K = 6, SOMA = 15 + 6 = 21
+
+// Quando K = 7, SOMA = 21 + 7 = 28
+
+// Quando K = 8, SOMA = 28 + 8 = 36
+
+// Quando K = 9, SOMA = 36 + 9 = 45
+
+// Quando K = 10, SOMA = 45 + 10 = 55
+
+// Quando K = 11, SOMA = 55 + 11 = 66
+
+// Quando K = 12, SOMA = 66 + 12 = 78
+
+// Quando K = 13, SOMA = 78
+
+// Então, ao final do processamento, o valor da variável SOMA será 91.
